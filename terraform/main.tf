f6489dfbbd3284981cb60dca9891d63c46673745
Jamie Pendergast <pendergastj@proton.me>
2023-08-15 09:56:07 +0100

    added gitignore and python requirements

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,6 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
     instance_type = "t2.micro"
-
     # This is the Amazon Machine Image for Ubuntu 
     ami = "ami-0eb260c4d5475b901"
 
@@ -23,3 +22,4 @@ resource "aws_instance" "app_server" {
         Name = "MinecraftServer"
     }
 }
+
--- terraform/s3_buckets.tf
+++ terraform/s3_buckets.tf
@@ -10,3 +10,29 @@ resource "aws_s3_bucket" "server_build" {
         Project = "Minecraft-Server"
     }
 }
+
+
+# Archive and upload the server build as a zip file
+# 
+
+# Step 1: Archive
+data "archive_file" "build_zip" {
+  type        = "zip"
+
+  # TODO:        Can these become variables?
+  output_path = "${path.module}/../build.zip"
+  source_dir  = "${path.module}/../build/"
+}
+
+# Step 2: Upload
+resource "aws_s3_object" "build_zip" {
+    bucket = aws_s3_bucket.server_build.bucket
+    key = "build.zip"
+    source      = data.archive_file.build_zip.output_path
+    source_hash = data.archive_file.build_zip.output_base64sha256
+
+    tags = {
+        Managed_by = "Terraform"
+        Project = "Minecraft-Server"
+    }
+}
